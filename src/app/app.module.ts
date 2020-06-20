d3a20d8085c74135bdb659e8de9ac7dde4119ebf
shekhars027@gmail.com <@Shashank1>
2020-06-20 09:40:54 +0530

    Added signup and sign in functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
+import { LoginComponent } from './user/login/login.component';
+import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ng6-toastr-notifications';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    ChatModule,
+    UserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '*', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+    ])
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { ToastrModule } from 'ng6-toastr-notifications';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+
+
+@NgModule({
+  declarations: [ChatBoxComponent],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    RouterModule.forChild([ 
+      { path: 'chat', component: ChatBoxComponent }
+    ]),
+  ]
+})
+export class ChatModule { }
