336ce0364396e88510d14b7ef57c5725d25424c7
shekhars027@gmail.com <@Shashank1>
2020-06-26 19:29:08 +0530

    added chat window and online status

--- src/app/chat/chat-box/chat-box.component.ts
+++ src/app/chat/chat-box/chat-box.component.ts
@@ -1,15 +1,121 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from './../../socket.service';
+import { AppService } from './../../app.service';
+
+import { Router } from '@angular/router';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+import { ToastrModule, ToastrManager } from 'ng6-toastr-notifications';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
-  styleUrls: ['./chat-box.component.css']
+  styleUrls: ['./chat-box.component.css'],
+  providers: [SocketService]
 })
 export class ChatBoxComponent implements OnInit {
 
-  constructor() { }
+  public authToken: any;
+  public userInfo: any;
+  public receiverId: any;
+  public receiverName: any;
+  public userList: any = [];
+  public disconnectedSocket: boolean;
 
-  ngOnInit(): void {
+
+  constructor(
+    public AppService: AppService,
+    public SocketService: SocketService,
+    public router: Router,
+    private toastr: ToastrManager
+  ) {
+
+    this.receiverId = Cookie.get('receiverId');
+
+    this.receiverName = Cookie.get('receiverName');
+    
+
+
+
   }
 
-}
+
+
+  ngOnInit() {
+
+    this.authToken = Cookie.get('authtoken');
+
+    this.userInfo = this.AppService.getUserInfoFromLocalstorage();
+
+    this.checkStatus();
+
+    this.verifyUserConfirmation();
+
+    this.getOnlineUserList()
+
+
+
+  }
+
+  public checkStatus: any = () => {
+
+    if (Cookie.get('authtoken') === undefined || Cookie.get('authtoken') === '' || Cookie.get('authtoken') === null) {
+
+      this.router.navigate(['/']);
+
+      return false;
+
+    } else {
+
+      return true;
+
+    }
+
+  } // end checkStatus
+
+
+
+  public verifyUserConfirmation: any = () => {
+
+    this.SocketService.verifyUser()
+      .subscribe((data) => {
+
+        this.disconnectedSocket = false;
+
+        this.SocketService.setUser(this.authToken);
+        this.getOnlineUserList()
+
+      });
+    }
+  
+  public getOnlineUserList :any =()=>{
+
+    this.SocketService.onlineUserList()
+      .subscribe((userList) => {
+
+        this.userList = [];
+
+        for (let x in userList) {
+
+          let temp = { 'userId': x, 'name': userList[x], 'unread': 0, 'chatting': false };
+
+          this.userList.push(temp);          
+
+        }
+        
+        console.log(this.userList);
+
+      }); // end online-user-list
+  }
+
+
+    
+
+
+
+
+
+
+
+  }
--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -93,6 +93,74 @@ export class SocketService {
 
     this.socket.emit('mark-chat-as-seen', userDetails);
 
-  }
+  } // end markChatAsSeen
+
+
+
+  // end events to be emitted
+
+  // chat related methods 
+
+  
+
+  public getChat(senderId, receiverId, skip): Observable<any> {
+
+    return this.http.get(`${this.url}/api/v1/chat/get/for/user?senderId=${senderId}&receiverId=${receiverId}&skip=${skip}&authToken=${Cookie.get('authtoken')}`)
+      .do(data => console.log('Data Received'))
+      .catch(this.handleError);
+
+  } // end logout function
+
+  public chatByUserId = (userId) => {
+
+    return Observable.create((observer) => {
+      
+      this.socket.on(userId, (data) => {
+
+        observer.next(data);
+
+      }); // end Socket
+
+    }); // end Observable
+
+  } // end chatByUserId
+
+  public SendChatMessage = (chatMsgObject) => {
+
+    this.socket.emit('chat-msg', chatMsgObject);
+
+  } // end getChatMessage
+
+
+  public exitSocket = () =>{
+
+
+    this.socket.disconnect();
+
+
+  }// end exit socket
+
+
+
+
+  private handleError(err: HttpErrorResponse) {
+
+    let errorMessage = '';
+
+    if (err.error instanceof Error) {
+
+      errorMessage = `An error occurred: ${err.error.message}`;
+
+    } else {
+
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+
+    } // end condition *if
+
+    console.error(errorMessage);
+
+    return Observable.throw(errorMessage);
+
+  }  // END handleError
 
 }
